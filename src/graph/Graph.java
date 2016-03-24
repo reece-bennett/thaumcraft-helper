f661424c9f5e29d18eacfd8e10a50724a09c7399
Reece Bennett <reece.bennett1@gmail.com>
2016-03-24 22:10:27 +0000

    Fix paths not being considered with duplicates

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -51,46 +51,24 @@ public class Graph {
     connect(name, new String[] { other });
   }
 
-  /*public ArrayList<Node> getPath(String start, String end) {
-    // Start by resetting the parents of all nodes in the graph
-    for (Node n : nodes.values()) {
-      n.setParent(null);
-    }
-
-    LinkedList<Node> frontier = new LinkedList<>();
-    LinkedList<Node> explored = new LinkedList<>();
-
-    // Start the search by adding the start node to the frontier
-    frontier.add(nodes.get(start));
-
-    while (!frontier.isEmpty()) {
-      Node current = frontier.removeFirst();
-
-      if (current.getName().equals(end)) {
-        // If the current node is the end point the search is complete, return the path
-        ArrayList<Node> path = new ArrayList<>();
-        Node parent = current;
-        while (!parent.getName().equals(start)) {
-          path.add(0, parent);
-          parent = parent.getParent();
-        }
-        path.add(0, parent);
-        return path;
-
-      } else if (!explored.contains(current)) {
-        // Else expand all children of the current node and if not already in frontier or explored,
-        // add them to the frontier
-        explored.add(current);
-        for (Node n : current.getNeighbours()) {
-          if (!frontier.contains(n) && !explored.contains(n)) {
-            n.setParent(current);
-            frontier.add(n);
-          }
-        }
-      }
-    }
-    return null;
-  }*/
+  /*
+   * public ArrayList<Node> getPath(String start, String end) { // Start by resetting the parents of all nodes in the
+   * graph for (Node n : nodes.values()) { n.setParent(null); }
+   * 
+   * LinkedList<Node> frontier = new LinkedList<>(); LinkedList<Node> explored = new LinkedList<>();
+   * 
+   * // Start the search by adding the start node to the frontier frontier.add(nodes.get(start));
+   * 
+   * while (!frontier.isEmpty()) { Node current = frontier.removeFirst();
+   * 
+   * if (current.getName().equals(end)) { // If the current node is the end point the search is complete, return the
+   * path ArrayList<Node> path = new ArrayList<>(); Node parent = current; while (!parent.getName().equals(start)) {
+   * path.add(0, parent); parent = parent.getParent(); } path.add(0, parent); return path;
+   * 
+   * } else if (!explored.contains(current)) { // Else expand all children of the current node and if not already in
+   * frontier or explored, // add them to the frontier explored.add(current); for (Node n : current.getNeighbours()) {
+   * if (!frontier.contains(n) && !explored.contains(n)) { n.setParent(current); frontier.add(n); } } } } return null; }
+   */
 
   public LinkedList<Node> getPath(String start, String end, int steps) {
     LinkedList<LinkedList<Node>> frontier = new LinkedList<>();
@@ -104,26 +82,24 @@ public class Graph {
     } else {
       frontier.add(new LinkedList<>(Arrays.asList(nodes.get(start))));
     }
-    
+
     while (!frontier.isEmpty()) {
       LinkedList<Node> path = frontier.removeFirst();
-      if (path.getLast().getName().equals(end) && path.size() > steps+2) {
+      if (path.getLast().getName().equals(end) && path.size() > steps + 2) {
         return path;
       } else {
         ArrayList<Node> neighbours = path.getLast().getNeighbours();
         for (Node n : neighbours) {
-          if (!path.contains(n)) {
-            @SuppressWarnings("unchecked")
-            LinkedList<Node> newPath = (LinkedList<Node>) path.clone();
-            newPath.add(n);
-            frontier.add(newPath);
-          }
+          @SuppressWarnings("unchecked")
+          LinkedList<Node> newPath = (LinkedList<Node>) path.clone();
+          newPath.add(n);
+          frontier.add(newPath);
         }
       }
     }
     return null;
   }
-  
+
   public int size() {
     return nodes.size();
   }

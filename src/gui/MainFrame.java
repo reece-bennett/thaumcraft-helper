c0e91ab8126fbb281c74f8e6c4fc7a4185014a95
Reece Bennett <reece.bennett1@gmail.com>
2016-03-24 11:52:53 +0000

    Add method to change look and feel of windows

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -2,8 +2,21 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import eclipsesource.json.Json;
+import eclipsesource.json.JsonArray;
+import eclipsesource.json.JsonObject;
+import eclipsesource.json.JsonObject.Member;
+import eclipsesource.json.JsonValue;
+import graph.Graph;
+import graph.Node;
 
 @SuppressWarnings("serial")
 public class MainFrame extends JFrame {
@@ -24,12 +37,30 @@ public class MainFrame extends JFrame {
     pack();
   }
 
-  public static void main(String[] args) {
-    
-    EventQueue.invokeLater(new Runnable() {
+  public static void setLookAndFeel(String name) {
+    try {
       
+      LookAndFeelInfo[] installed = UIManager.getInstalledLookAndFeels();
+      for (LookAndFeelInfo info : installed) {
+        System.out.println(info.getClassName());
+        if (info.getName().contains(name)) {
+          UIManager.setLookAndFeel(info.getClassName());
+        }
+      }
+      
+    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+        | UnsupportedLookAndFeelException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args) {
+
+    EventQueue.invokeLater(new Runnable() {
+
       @Override
       public void run() {
+        setLookAndFeel("Nimbus");
         MainFrame frame = new MainFrame();
         frame.setVisible(true);
       }
